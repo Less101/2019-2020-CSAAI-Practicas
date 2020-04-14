cadfc89b34fb7c79c401b58affe0b80848bf7535
Jorge <j.delgadov@alumnos.urjc.es>
2020-04-14 13:12:39 +0200

    un poco más

--- P4/video_01.js
+++ P4/video_01.js
@@ -1,15 +1,37 @@
 console.log("Ejecutando JS...");
 
 //----- Obtener elemento de video y configurarlo
+
+const video0 = document.getElementById("video0")
+
+
+
 const video1 = document.getElementById("video1")
 video1.width=200;  //-- Tamaño de la pantalla de video
 video1.height=100;
 
+const video2 = document.getElementById("video2")
+video2.width=200;  //-- Tamaño de la pantalla de video
+video2.height=100;
+const video3 = document.getElementById("video3")
+video3.width=200;  //-- Tamaño de la pantalla de video
+video3.height=100;
+
+
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
-video1.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
+video0.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
+video1.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
+video2.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
+video3.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
 
 //-- Obtener los botones
+const play = document.getElementById("play")
+const stop = document.getElementById("stop")
+
+const play = document.getElementById("play")
+const stop = document.getElementById("stop")
+
 const play = document.getElementById("play")
 const stop = document.getElementById("stop")
 
@@ -18,6 +40,10 @@ play.onclick = () => {
   console.log("Click!");
   video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
   video1.play();
+  video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
+  video2.play();
+  video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
+  video3.play();
 };
 
 //-- Funcion de retrollamada del boton de parar
