5c883dfd49148a770fca2faef607c63324c3d5a6
Jorge <j.delgadov@alumnos.urjc.es>
2020-05-23 20:59:36 +0200

    Casi está...

--- P4/video_01.js
+++ P4/video_01.js
@@ -2,55 +2,63 @@ console.log("Ejecutando JS...");
 
 //----- Obtener elemento de video y configurarlo
 
-const video0 = document.getElementById("video0")
+const video0 = document.getElementById("video0");
+video0.width=400; //-- pantalla grande
+video0.height=200;
 
 
-
-const video1 = document.getElementById("video1")
+const video1 = document.getElementById("video1");
 video1.width=200;  //-- Tamaño de la pantalla de video
 video1.height=100;
 
-const video2 = document.getElementById("video2")
+const video2 = document.getElementById("video2");
 video2.width=200;  //-- Tamaño de la pantalla de video
 video2.height=100;
-const video3 = document.getElementById("video3")
+
+const video3 = document.getElementById("video3");
 video3.width=200;  //-- Tamaño de la pantalla de video
 video3.height=100;
 
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
-video0.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
 video1.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
 video2.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
 video3.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
 
 //-- Obtener los botones
-const play = document.getElementById("play")
-const stop = document.getElementById("stop")
+const play1 = document.getElementById("selector1")
+const play2 = document.getElementById("selector2")
+const play3 = document.getElementById("selector3")
 
-const play = document.getElementById("play")
-const stop = document.getElementById("stop")
-
-const play = document.getElementById("play")
-const stop = document.getElementById("stop")
 
 //-- Función de retrollamada del botón de ver
-play.onclick = () => {
-  console.log("Click!");
+play1.onclick = () => {
+  console.log("Click_1!");
   video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
+  video0.src = video1.src;
   video1.play();
+  video0.play();
+}
+play2.onclick = () => {
+  console.log("Click_2!");
   video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
+  video0.src = video2.src;
   video2.play();
+  video0.play();
+}
+play3.onclick = () =>{
+  console.log("Click_3!");
   video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
+  video0.src = video3.src;
   video3.play();
-};
+  video0.play();
+}
 
 //-- Funcion de retrollamada del boton de parar
-stop.onclick = () => {
-  video1.pause();
+//stop.onclick = () => {
+//  video1.pause();
 
   //-- Quitar la fuente de video, para que se muestre la
   //-- imagen definida en el atributo poster
-  video1.src=null;
-}
+  video0.src=null;
