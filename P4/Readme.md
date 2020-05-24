954a256089c74d2c7864685f995a1c83e22f7e5f
Jorge <j.delgadov@alumnos.urjc.es>
2020-05-24 15:55:27 +0200

    P5 acabada, con mejoras,

--- P4/Readme.md
+++ P4/Readme.md
@@ -1,1 +1,3 @@
 # Práctica 4
+Añadida una imagen estática de una carta de ajuste en la pantalla principal.Que se verá de inicio.
+Cuando se pulse play en cada uno de los players se visionará su vídeo, hasta el infinito, ya que está la opción loop activada en cada uno de los players. Además se verá en la pantalla principal la señal del player que esté repoduciendo, deteniéndose el reso de los players.
--- P4/video_01b.js
+++ P4/video_01b.js
@@ -6,6 +6,8 @@ const video0 = document.getElementById("video0");
 video0.width=400; //-- pantalla grande
 video0.height=200;
 
+
+
 const video1 = document.getElementById("video1");
 video1.width= 200;  //-- Tamaño de la pantalla de video
 video1.height=100;
@@ -26,59 +28,50 @@ video1.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/
 video2.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
 video3.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png"
 
+
 //-- Obtener los botones
-const play1 = document.getElementById("selector1")
-const play2 = document.getElementById("selector2")
-const play3 = document.getElementById("selector3")
+//const play1 = document.getElementById("selector1")
+//const play2 = document.getElementById("selector2")
+//const play3 = document.getElementById("selector3")
 
 
-//-- Función de retrollamada del botón de ver
-play1.onclick = () => {
-  console.log("Click_1!");
-  video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
-  video0.src = video1.src;
+//--Función de retrollamada del botón de ver
+//play1.onclick = () => {
+  //console.log("Click_1!");
 
-  video1.play();
-  video0.play();
-  
-  video2.pause();
-  video3.pause();
+if( video1.controls == true){
+    video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
+    video0.src = video1.src;
+    video0.play();
+    video1.style.border = "solid red 5px";
+    video2.style.border = "";
+    video3.style.border = "";
+    video0.style.border = "";
+  }
 
-  video1.style.border = "solid red 5px";
-  video2.style.border = "";
-  video3.style.border = "";
-  video0.style.border = "";
 
-}
-play2.onclick = () => {
-  console.log("Click_2!");
+
   video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
   video0.src = video2.src;
-  video2.play();
-  video0.play();
-
-  video1.pause();
-  video3.pause();
-
   video1.style.border = "";
   video2.style.border =  "solid red 5px";
   video3.style.border = "";
   video0.style.border = "";
 
-}
-play3.onclick = () =>{
-  console.log("Click_3!");
+//play3.onclick = () =>{
+  //console.log("Click_3!");
   video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
   video0.src = video3.src;
-  video3.play();
-  video0.play();
-
-  video1.pause();
-  video2.pause();
-
   video1.style.border = "";
   video2.style.border = "";
   video3.style.border = "solid red 5px";
   video0.style.border = "";
-}
-  video0.src=null;
+
+
+//-- Funcion de retrollamada del boton de parar
+//stop.onclick = () => {
+//  video1.pause();
+
+  //-- Quitar la fuente de video, para que se muestre la
+  //-- imagen definida en el atributo poster
+  //video0.src=null;
